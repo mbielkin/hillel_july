8e446431f697207d1cd4b7cf8847cee441f60cab
Maksym Teacher <maksym.belkin@gmail.com>
2025-07-17 20:27:43 +0300

    updates js logic

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-
-function f() {
-  for (let i = 0; i < 10; i++) {
-    if (i % 2 === 0) {
-      let userValue = prompt('Enter your value:')
-      console.log(userValue);
-    }
-  }
-}
+console.log('Hello, world!');
