8460a4ed7aaddb4951b136742929d46f95eafef4
Maksym Teacher <maksym.belkin@gmail.com>
2025-07-17 20:58:15 +0300

    my feature developed

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ console.log('Hello, world!');
 let message = 'Git is awesome';
 console.log(message);
 
+let x = 1;
+let b = 2;
+const c = x + b;
+console.log(c);
+
 console.log('update from user 2');
 console.log('update from user1');
-
-// merge conflict
-// resolve merge conflict
